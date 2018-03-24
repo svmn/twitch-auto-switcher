e112e321f86d3700cc346cbab8bac205d08feb97
red samara <root@tuzach.in>
2018-03-24 18:56:47 +0200

    Add hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path'),
   webpack = require('webpack'),
   ExtractTextPlugin = require('extract-text-webpack-plugin'),
   CopyWebpackPlugin = require("copy-webpack-plugin"),
+  ChromeExtensionReloader = require('webpack-chrome-extension-reloader'),
   dir_js = path.resolve(__dirname, 'src/scripts'),
   dir_build = path.resolve(__dirname, 'dist');
 
@@ -32,6 +33,15 @@ module.exports = {
       { from: 'src/manifest.json' },
       { from: 'src/images', to: 'images' }
     ]),
+    new ChromeExtensionReloader({
+      port: 9090, // Which port use to create the server
+      reloadPage: true, // Force the reload of the page also
+      entries: { //The entries used for the content/background scripts
+        contentScript: 'contentscript', //Use the entry names, not the file name or the path
+        background: 'background',
+        popup: 'popup',
+      }
+    }),
     new webpack.NoEmitOnErrorsPlugin()
   ],
 
